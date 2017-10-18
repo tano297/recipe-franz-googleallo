58a3d81b2c83a35939b46a100a8befb79b806685
Luis Rodriguez <luis@silocitylabs.com>
2017-10-18 13:28:17 -0400

    notification badges

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-module.exports = Franz => class Discord extends Franz {
-    overrideUserAgent() {
-        //const useragent = window.navigator.userAgent;
-        //return useragent.replace('Chrome/52', 'Chrome/61');
-        //return "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36";
-		return window.navigator.userAgent;
-    }
-}; 
+"use strict";
+
+// just pass through Franz
+
+module.exports = function (Franz) {
+  return Franz;
+};
