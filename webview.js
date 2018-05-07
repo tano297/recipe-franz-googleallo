cf4f5f1831768366717d295eecfd3e22ba4715ec
amilioto <tano.297@gmail.com>
2018-05-07 14:26:51 +0200

    allo -> wunderlist

--- webview.js
+++ webview.js
@@ -2,12 +2,14 @@ module.exports = (Franz) => {
   const getMessages = function getMessages() {
     let count = 0;
 
-    const elements = document.querySelectorAll('.unreadCount');
-    for (let i = 0; i < elements.length; i += 1) {
-      if (parseInt(elements[i].innerHTML, 10) !== 0) {
-        count += 1;
-      }
-    }
+    // get all the containers with overdue
+    var overdue_inbox_all = document.querySelectorAll('.overdue')[3];
+    item_text = overdue_inbox_all.getElementsByTagName("span")[1].innerText;
+    if (item_text.localeCompare("All") == 0) {
+      // if it is the "all" list, go for it
+      local_count = parseInt(overdue_inbox_all.getElementsByTagName("span")[2].innerText);
+      count += local_count;
+    };
 
     Franz.setBadge(count);
   };
